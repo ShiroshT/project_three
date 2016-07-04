509923a1bab15fb6f660687fbb4f9a96c63fdf7b
Shiroshana Tissera <shiroshana@gmail.com>
2016-07-04 21:12:49 +0100

    files connected

---
+++ api_store/apigateway.py
@@ -0,0 +1,5 @@
+from api_item1 import api_item1
+
+
+def api_gateway():
+    api_item1.api_item1('Brexit')
---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+from api_store import apigateway
+
+
+def main():
+    apigateway.api_gateway()
+
+
+
+main()
