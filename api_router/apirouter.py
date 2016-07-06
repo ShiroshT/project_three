1ffea24e529ea9470a077ec62d03756df4887753
Shiroshana Tissera <shiroshana@gmail.com>
2016-07-06 20:43:26 +0100

    adding API result cleaner1

--- api_router/apirouter.py
+++ api_router/apirouter.py
@@ -1,4 +1,46 @@
-from api_store import api_item1 # Ny times API call 
+from api_store import api_item1 # Ny times API call
+from cleaner import api_result_cleaner # cleanup API results
 
+
+
+
+
+
+
+
+
+
+
+'''----------------------------------------------
+    Divert the results from the API Clean up
+------------------------------------------------'''
+def api_result_cleaner(result):
+
+
+
+
+
+
+
+
+
+
+
+
+'''----------------------------------------------
+    Send the
+------------------------------------------------'''
+
+
+
+
+
+'''---------------------------------------------
+    Call APIs from all the news sources.
+------------------------------------------------'''
 def api_router(query):
-    api_item1.api_item1(query)
+    qu_results= api_item1.api_item1(query)
+    api_result_cleaner(qu_results)
+
+
+
